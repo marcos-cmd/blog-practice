bd17cb5c387d8bac0b305faeeaa4d5d94744a8ad
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-10-15 21:17:54 -0700

    added pages

--- my-blog/src/pages/HomePage.js
+++ my-blog/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Homepage = () => (
-    <React.Fragment>
+const HomePage = () => (
+    // <React.Fragment> is used instead of wrapping in Divs
+    <>
         <h1>Hello, welcome to my blog!</h1>
         <p>Welcome to my blog. Velit magna dolore id sit pariatur eu commodo quis aliqua ex consectetur occaecat. Labore id est do in excepteur ex enim pariatur. Cillum aliquip cupidatat ex incididunt eu occaecat sint aute. Laboris sunt anim velit ipsum pariatur cupidatat nulla. Minim non magna incididunt officia id occaecat nulla consectetur ad reprehenderit consectetur et. Nulla culpa elit laboris anim ipsum laborum adipisicing officia ullamco in.</p>
         <p>Pariatur in labore duis in dolore cillum sunt. Et mollit deserunt ullamco consequat non id qui velit laboris et. Velit est labore reprehenderit do proident enim tempor commodo consectetur ad commodo aute minim non. Ipsum consectetur ipsum aliqua minim irure dolor pariatur tempor ea id officia deserunt officia est. Est commodo esse tempor ut laborum anim elit pariatur officia voluptate.</p>
-    </React.Fragment>
+    </>
 );
-export default Homepage;
+
+export default HomePage;
